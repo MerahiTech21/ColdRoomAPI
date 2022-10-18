117b9aa0e422239da2d19ab52932a62cc8ef0b1b
Alemu Tebkew <alemteb1010@gmail.com>
2022-10-17 14:37:52 -1200

    first commit

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const Sequelize=require('sequelize')
+
+const sequelize= new Sequelize('cold-room','root','',{dialect:'mysql',host:'localhost',port:'3308',})
+
+try {
+    sequelize.authenticate();
+   console.log('Sequelize Connection has been established successfully.');
+ } catch (error) {
+   console.error('Sequelize Unable to connect to the database:', error);
+ }
+ 
+ const db ={}
+
+ db.sequelize=sequelize
+ db.Sequelize=Sequelize
+
+module.exports ={db,sequelize};
